4fa878007ce5e68d09466904cdcade81f47dd88c
Fabian Zach <fabian.zach@ul.com>
2024-02-14 11:27:11 +0100

    feat: Filter by OAuth key instead of email

--- back/taiga_contrib_oidc_auth/oidc.py
+++ back/taiga_contrib_oidc_auth/oidc.py
@@ -28,19 +28,18 @@ from taiga.auth.signals import user_registered as user_registered_signal
 class TaigaOIDCAuthenticationBackend(OIDCAuthenticationBackend):
 
     AUTHDATA_KEY = os.getenv("OIDC_AUTHDATA_KEY", "oidc")
+    AUTHDATA_CLAIM = os.getenv("OIDC_CLAIM_AUTHDATA", "sub")
     EMAIL_CLAIM = os.getenv("OIDC_CLAIM_EMAIL", "email")
     FULLNAME_CLAIM = os.getenv("OIDC_CLAIM_FULLNAME", "name")
     USERNAME_CLAIM = os.getenv("OIDC_CLAIM_USERNAME", "nickname")
 
     def filter_users_by_claims(self, claims):
-        email = claims.get(self.EMAIL_CLAIM)
-        if not email:
-            return self.UserModel.objects.none()
+        auth_id = claims.get(self.AUTHDATA_CLAIM, self.get_username(claims))
 
         AuthData = apps.get_model("users", "AuthData")
         try:
             auth_data = AuthData.objects.filter(
-                key=self.AUTHDATA_KEY, user__email__iexact=email
+                key=self.AUTHDATA_KEY, value=auth_id
             )
             return [ad.user for ad in auth_data]
 
@@ -60,18 +59,19 @@ class TaigaOIDCAuthenticationBackend(OIDCAuthenticationBackend):
 
         username = self.get_username(claims)
         full_name = claims.get(self.FULLNAME_CLAIM, username)
+        auth_id = claims.get(self.AUTHDATA_CLAIM, username)
 
         AuthData = apps.get_model("users", "AuthData")
         try:
             # User association exist?
-            auth_data = AuthData.objects.get(key=self.AUTHDATA_KEY, value=username)
+            auth_data = AuthData.objects.get(key=self.AUTHDATA_KEY, value=auth_id)
             user = auth_data.user
         except AuthData.DoesNotExist:
             try:
                 # Is a user with the same email?
                 user = self.UserModel.objects.get(email=email)
                 AuthData.objects.create(
-                    user=user, key=self.AUTHDATA_KEY, value=username, extra={}
+                    user=user, key=self.AUTHDATA_KEY, value=auth_id, extra={}
                 )
             except self.UserModel.DoesNotExist:
                 # Create a new user
@@ -79,7 +79,7 @@ class TaigaOIDCAuthenticationBackend(OIDCAuthenticationBackend):
                     email=email, username=username, full_name=full_name
                 )
                 AuthData.objects.create(
-                    user=user, key=self.AUTHDATA_KEY, value=username, extra={}
+                    user=user, key=self.AUTHDATA_KEY, value=auth_id, extra={}
                 )
 
                 send_register_email(user)
