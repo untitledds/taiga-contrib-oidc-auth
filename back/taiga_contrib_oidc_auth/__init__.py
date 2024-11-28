449e5d4d17b82cddcce789389b987342afe99539
Aleksey <untitledds@users.noreply.github.com>
2024-11-28 03:17:35 +0300

    Update __init__.py

--- back/taiga_contrib_oidc_auth/__init__.py
+++ back/taiga_contrib_oidc_auth/__init__.py
@@ -15,4 +15,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __version__ = (0, 0, 1)
-
+default_app_config = "taiga_contrib_oidc_auth.apps.TaigaContribOIDCAuthAppConfig"
